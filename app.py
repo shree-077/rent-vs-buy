6b6baa0203272258f76dbf3619e2807a30bfdacc
shree-077 <shreyas077@gmail.com>
2026-02-16 01:31:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,28 +19,7 @@ down_payment = st.sidebar.number_input(
     step=50_000
 )
 st.sidebar.subheader("Loan Interest Rate")
-
-loan_rate_input = st.sidebar.number_input(
-    "Interest Rate (%)",
-    min_value=5.0,
-    max_value=15.0,
-    value=8.0,
-    step=0.1,
-    key="loan_rate_input"
-    )
-
-loan_rate_slider = st.sidebar.slider(
-    "Adjust Interest Rate",
-    min_value=5.0,
-    max_value=15.0,
-    value=loan_rate_input,
-    step=0.1,
-    key="loan_rate_slider"
-    )
-
-# single source of truth
-loan_rate = loan_rate_slider / 100
-loan_tenure = st.sidebar.slider("Loan Tenure (Years)", 5, 30, 20)
+loan_rate = st.sidebar.slider("Loan Interest (%)", 5.0, 12.0, 8.0, step=0.05) / 100)
 
 st.sidebar.header("Rent")
 starting_rent = st.sidebar.number_input("Monthly Rent (₹)", 0, 5_00_000, 20_000)
